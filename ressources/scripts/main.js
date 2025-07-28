4d06fe8a160230a67cf1313c244169a5ec45bdf0
Bouquinator <123673699+Bouquinator@users.noreply.github.com>
2025-07-28 17:11:18 +0200

    Delete main.js

--- ressources/scripts/main.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.toggleBtn').forEach(btn => {
-    btn.addEventListener('click', () => {
-    const contentDiv = btn.closest('.content').querySelector('.hidden-content');
-    contentDiv.classList.toggle('show');
-    });
-});
